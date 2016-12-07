cf75b6ae32d003b99a40d101eb135949487cb808
Дмитрий Хекк <d.khekk@petrosoft.su>
2016-12-07 11:59:51 +0300

    answers marked by "Ответ:"

--- src/java/excell/HtmlHelper.java
+++ src/java/excell/HtmlHelper.java
@@ -1,6 +1,7 @@
 package excell;
 
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.File;
@@ -9,10 +10,12 @@ import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Map;
 
+import static java.util.stream.Collectors.joining;
+
 /**
  * Created by khekk on 06.12.16
  */
-public class HtmlHelper {
+class HtmlHelper {
 
     public Map<String, String> getQuestions(String filepath) throws IOException {
         File file = new File(filepath);
@@ -20,15 +23,18 @@ public class HtmlHelper {
         return getQuestions(file);
     }
 
-    public Map<String, String> getQuestions(File file) throws IOException {
+    Map<String, String> getQuestions(File file) throws IOException {
         Elements elements = Jsoup.parse(file,
                 Charset.defaultCharset().name())
                 .select("div.question-result");
         Map<String, String> map = new HashMap<>();
         elements.forEach(element -> {
-            String sb = element.select("div.question-text").first().ownText() + "\n\n" +
+            String question = element.select("div.question-text").first().ownText() + "\n\n" +
                     Jsoup.parse(element.select("code.java").html()).text().replace("{", "{\n").replace(";", ";\n").replace("}", "}\n");
-            map.put(sb, element.select("p.correct").text());
+            String answers = element.select("p.correct").stream()
+                    .map(Element::ownText)
+                    .collect(joining("\nОтвет: ", "Ответ: ", "."));
+            map.put(question, answers);
         });
         return map;
     }
