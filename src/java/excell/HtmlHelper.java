13fa97f3a6880d8e5d19d7c81207352aab115b5c
Дмитрий Хекк <d.khekk@petrosoft.su>
2016-12-08 14:30:29 +0300

    map changed to set<question>

--- src/java/excell/HtmlHelper.java
+++ src/java/excell/HtmlHelper.java
@@ -7,8 +7,8 @@ import org.jsoup.select.Elements;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 import static java.util.stream.Collectors.joining;
 
@@ -17,25 +17,25 @@ import static java.util.stream.Collectors.joining;
  */
 class HtmlHelper {
 
-    public Map<String, String> getQuestions(String filepath) throws IOException {
+    public Set<Question> getQuestions(String filepath) throws IOException {
         File file = new File(filepath);
 
         return getQuestions(file);
     }
 
-    Map<String, String> getQuestions(File file) throws IOException {
+    Set<Question> getQuestions(File file) throws IOException {
         Elements elements = Jsoup.parse(file,
                 Charset.defaultCharset().name())
                 .select("div.question-result");
-        Map<String, String> map = new HashMap<>();
+        Set<Question> result = new HashSet<>();
         elements.forEach(element -> {
-            String question = element.select("div.question-text").first().ownText() + "\n\n" +
-                    Jsoup.parse(element.select("code.java").html()).text().replace("{", "{\n").replace(";", ";\n").replace("}", "}\n");
+            String question = element.select("div.question-text").first().ownText();
+            String code = element.select("code.java").text().replace("{", "{\n").replace(";", ";\n").replace("}", "}\n");
             String answers = element.select("p.correct").stream()
                     .map(Element::ownText)
                     .collect(joining("\nОтвет: ", "Ответ: ", "."));
-            map.put(question, answers);
+            result.add(new Question(question, code, answers));
         });
-        return map;
+        return result;
     }
 }
